c77c74e7884209372140113dcc354861744c1996
OBoers <o.boers@gmail.com>
2015-06-10 14:43:04 +0200

    Server

--- Server/add_plant.php
+++ Server/add_plant.php
@@ -1,27 +1,27 @@
 <?php
-//start een sessie
+// Start een sessie
 session_start();
 
-//zet sessie variabelen
+// Zet sessie variabelen
 $user = $_SESSION['username'];
 $id = $_SESSION['user_id'];
 $plant_id = $_SESSION['plant_id'];
 
-//connect met de server/database
+// Connect met de server/database
 $connect = mysqli_connect('localhost', 'plant', '$_Tan1900', 'plant');
 
-//query om aantal gegevens uit de type tabel te halen
+// Query om aantal gegevens uit de type tabel te halen
 $query2 = "SELECT name, type_id FROM type";
 
-//run de query
+// Run de query
 $result2 = mysqli_query($connect, $query2);
 
-//als er minder rijen worden teruggegeven dan de lengte, geef dan een foutmelding
-if (mysqli_num_rows($result2) < strlen($result2))
-{
+// Als er minder rijen worden teruggegeven dan de lengte, geef dan een foutmelding
+if (mysqli_num_rows($result2) < strlen($result2)) {
     echo 'Er ging iets mis met het ophalen van de planttypes';
 }
 ?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
 <html>
     <head>
@@ -53,78 +53,86 @@ if (mysqli_num_rows($result2) < strlen($result2))
                     </div>
                 </div>
             </li>
+
             <?php
-            //als er geen sessie is gestart, toon dan de onderstaande link in de header
-            if(!$_SESSION)
-            {
-            ?>
-            <li>	
-                <a href='register.php' class='nav-item'>Registreer</a>
-            </li>
-            <?php
+// Als er geen sessie is gestart, toon dan de onderstaande link in de header
+            if (!$_SESSION) {
+                ?>
+
+                <li>	
+                    <a href='register.php' class='nav-item'>Registreer</a>
+                </li>
+
+                <?php
             }
             ?>
+
             <li>
         </ul>
+
         <?php
-        //als er geen sessie is gestart, toon dan de deze tekst
-        if(!$_SESSION)
-        {
-        ?>
-        <div class='title'><p>Project Plant</p>
-        </div>
-        <?php
+// Als er geen sessie is gestart, toon dan de deze tekst
+        if (!$_SESSION) {
+            ?>
+
+            <div class='title'>
+                <p>Project Plant</p>
+            </div>
+
+            <?php
         }
-        //als er wel een sessie is gestart, toon dan de onderstaande tekst in de header
-        else
-        {
-        ?>
-            <div class='title'><p>Ingelogd als <?php echo $user?></p>
-        </div>
-        <?php
+// Als er wel een sessie is gestart, toon dan de onderstaande tekst in de header
+        else {
+            ?>
+
+            <div class='title'>
+                <p>Ingelogd als <?php echo $user ?></p>
+            </div>
+
+            <?php
         }
         ?>
     </nav>	
     <div class='content'>
         <div class='login'>
             <form action='add_plant.php' method='POST'>
-        <p>Vul de onderstaande gegevens in om de registratie te voltooien</p>
-        </br>
-        <table>
-            <tr>
-                <td>Plantnaam</td>
-                <td><input type='text' name='plant_name'></td>
-            </tr>
-            <tr>
-                <td>Plant type</td>
-                <td>
-                    
-                    <!-- dropdown menu met daarin de plant types!-->
-                    <select name="option_value" style="width: 100%">
-                        <?php 
-                        while ($row2 = mysqli_fetch_assoc($result2))
-                        {
-                        echo '<option value="' . $row2['type_id'] . '">' . $row2['name'] . '</option>';
-                        }
-                        ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan='2'><input type='submit' name='submit' value='Registreer'></td>
-            </tr>
-        </table>
+                <p>Vul de onderstaande gegevens in om de registratie te voltooien</p>
+                </br>
+                <table>
+                    <tr>
+                        <td>Plantnaam</td>
+                        <td><input type='text' name='plant_name'></td>
+                    </tr>
+                    <tr>
+                        <td>Plant type</td>
+                        <td>
+
+                            <!-- Dropdown menu met daarin de plant types!-->
+                            <select name="option_value" style="width: 100%">
+                                <?php
+                                while ($row2 = mysqli_fetch_assoc($result2)) {
+                                    echo '<option value="' . $row2['type_id'] . '">' . $row2['name'] . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'><input type='submit' name='submit' value='Registreer'></td>
+                    </tr>
+                </table>
         </div>
         <div class="footer">
             <div id="footerlinks">&copy; 2015</div>
             <div id="footerrechts">
-            <?php
-            //als er een session is gestart, toon de log uit knop
-            if($_SESSION)
-            {
-                echo "<a class='test' href='http://casnetwork.tk/plant/logout.php' class='nav-item'>Log uit</a>"; 
-            }
-            ?>
+
+                <?php
+//Als er een session is gestart, toon de log uit knop
+                if ($_SESSION) {
+                    echo "<a class='test' href='http://casnetwork.tk/plant/logout.php' class='nav-item'>Log uit</a>";
+                }
+                ?>
+
             </div>
         </div>
     </div>
@@ -134,35 +142,31 @@ if (mysqli_num_rows($result2) < strlen($result2))
 </html>
 <?php
 //als er een POST REQUEST wordt gedaan
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
 //als er op de submit knop wordt geklikt    
-if (isset($_POST['submit']))
-{
-    //zet sessie variabele en maak een $errors array aan
-    $type_id = $_POST['option_value'];
-    $errors = [];
+    if (isset($_POST['submit'])) {
+        //zet sessie variabele en maak een $errors array aan
+        $type_id = $_POST['option_value'];
+        $errors = [];
 
-    //als plant_name niet leeg is, run de onderstaande code
-    if (!(empty($_POST['plant_name'])))
-    {   
-        //zet een sessie variabele
-        $plant = $_POST['plant_name'];
+        //als plant_name niet leeg is, run de onderstaande code
+        if (!(empty($_POST['plant_name']))) {
+            //zet een sessie variabele
+            $plant = $_POST['plant_name'];
 
-        //query om de gegevens aan de database toe te voegen
-        $query = "INSERT INTO plant VALUES('NULL', '$plant', '$type_id', '$id')";
+            //query om de gegevens aan de database toe te voegen
+            $query = "INSERT INTO plant VALUES('NULL', '$plant', '$type_id', '$id')";
 
-        //run de query
-        $result = mysqli_query($connect, $query) or die(mysqli_error($connect));
+            //run de query
+            $result = mysqli_query($connect, $query) or die(mysqli_error($connect));
 
-        //sluit de connectie met de database
-        mysqli_close($connect);
+            //sluit de connectie met de database
+            mysqli_close($connect);
 
-        if($query)
-        {
-            echo 'Je plant is toegevoegd aan de database!';
-        }
+            if ($query) {
+                echo 'Je plant is toegevoegd aan de database!';
+            }
         }
     }
 }
